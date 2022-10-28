b218fdf4c52b989b335e054e25a2dd1f3500b23b
Kenta Bilotta <kentabilotta20@gmail.com>
2022-10-28 15:11:59 +0200

    terzo commit: es pari/dispari fatto

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ if (parola == reverse) {
     output.innerHTML = parola + " non è polindroma";
 }
 
+// chiedere all'utente pari o disparie di inserire un numero; generiamo un numero random per il computer; facciamo la somma dei due numeri e stabilire chi è il vincitore
+
+let userPariDispari = prompt("pari o dispari?");
+document.getElementById("user-pd").innerHTML = userPariDispari;
+
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+document.getElementById("user-number").innerHTML = userNumber;
+
+function getRandomInteger (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+let computerNumber = getRandomInteger (1, 5);
+document.getElementById("computer-number").innerHTML = computerNumber;
+
+let somma = userNumber + computerNumber;
+document.getElementById("somma").innerHTML = somma;
+
+if ((somma % 2 == 0 && userPariDispari == "pari") || (somma % 2 !== 0 && userPariDispari == "dispari")) {
+    document.getElementById("winner").innerHTML = "Hai vinto!"
+} else {
+    document.getElementById("winner").innerHTML = "Hai perso"
+}
